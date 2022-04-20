c25e9183fe77072135834cd971fbdd46b9fe8606
Imran Hossain <hi@imranh.org>
2022-04-19 21:50:50 -0400

    Use itty-router for routing requests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
+import { Router } from 'itty-router'
+
 // This gets bound to the KV namespace
 declare const MARKS: KVNamespace
 
-const handleRequest = async (request: Request): Promise<Response> => {
-  const url = new URL(request.url);
+const router = Router()
+
+router.get('/:key', async (request) => {
   // Get a link from KV
-  const key = url.pathname.split('/')[1];
-  const link = await MARKS.get(key);
+  const url = new URL(request.url)
+  const key = url.pathname.split('/')[1]
+  const link = await MARKS.get(key)
 
   // Give a 404 if the link doesn't exist
-  if (link == null) return new Response("No link with this key.", { status: 404 });
+  if (link == null) return new Response('No link with this key.', { status: 404 })
 
   return Response.redirect(link, 301)
-}
+})
+// Only GETs allowed for keys
+router.all('/:key', async () => new Response('Method not allowed.', { status: 405 }))
+
+router.all('*', () => new Response('Not Found.', { status: 404 }))
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(router.handle(event.request, event))
 })
