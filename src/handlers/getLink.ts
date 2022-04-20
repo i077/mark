fea88c083211bc04dabd565435cd0a0f58250d63
Imran Hossain <hi@imranh.org>
2022-04-20 07:40:03 -0400

    Use parameter object in get endpoint

--- src/handlers/getLink.ts
+++ src/handlers/getLink.ts
@@ -5,8 +5,8 @@ declare const MARKS: KVNamespace
 
 export const getLink = async (request: Request): Promise<Response> => {
   // Get a link from KV
-  const url = new URL(request.url)
-  const key = url.pathname.split('/')[1]
+  const key = request.params?.key
+  if (key == null) return new Response('No key provided.', { status: 400 })
   const link = await MARKS.get(key)
 
   // Give a 404 if the link doesn't exist
